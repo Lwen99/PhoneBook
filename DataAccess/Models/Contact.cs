334f4eaacec6b84a492da85d64794506fe0d8dc1
Wenrui Li <liwenrui@msu.edu>
2024-10-16 17:34:32 -0400

    Add project files.

---
+++ DataAccess/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public string?Name { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email address format")]
+        public string? Email { get; set; } = "";
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be exactly 10 digits and numeric")]
+        public string PhoneNumber { get; set; } = "";
+    }
+}
---
+++ PhoneBook/Controllers/ContactController.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess.Models;
+using DataAccess.ORMs;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace PhoneBook.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Contact> contacts = _context.Contacts.ToList();
+            return View(contacts);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Contact contact = _context.Contacts.FirstOrDefault(a => a.Id == id);
+            if (contact != null)
+            {
+                _context.Contacts.Remove(contact);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                Contact contact = new Contact
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+                _context.Contacts.Add(contact);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+        }
+        [HttpGet("Contact/Update/{Id}")]
+        public IActionResult Update([FromRoute]int id)
+        {
+            Contact contact = _context.Contacts.FirstOrDefault(s => s.Id == id);
+            if (contact != null)
+            {
+                return View(contact);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
